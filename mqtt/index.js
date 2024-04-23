8cf9d8a3e9ceeda7370477c06545c17c435af1ba
HannesGingby <hannes.gingby@hitachigymnasiet.se>
2024-04-23 13:14:01 +0200

    Cleaned up the site + added a connection info section

--- mqtt/index.js
+++ mqtt/index.js
@@ -1,33 +1,64 @@
 const express = require('express');
 const http = require('http');
-
-// Import the connect function from mqttConnect.js
-const { connect } = require('./mqttConnect');
+var mqtt = require('mqtt')
 
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: "http://127.0.0.1:5500", // The origins of which are allowed
+    // The origins of which are allowed
+    origin: "http://127.0.0.1:5500",
     methods: ['GET', 'POST']
   }
 });
+
+const clusterURL = '24481123c0884e459cd76ccc6ca6d326.s1.eu.hivemq.cloud';
+const port = 8883;
+const protocol = 'mqtts';
+
+function connect(clientID, username, password) {
+  // Initialize the MQTT client
+  var client = mqtt.connect({
+    host: clusterURL,
+    port: port,
+    protocol: protocol,
+    username: username,
+    password: password
+  });
+
+  // Setup the callbacks
+  client.on('connect', function () {
+    console.log('Connected');
+  });
+
+  client.on('error', function (error) {
+    console.log(error);
+  });
+
+  client.on('message', function (topic, message) {
+    // Called each time a message is received
+    console.log('Received message:', topic, message.toString());
+  });
+}
 
 // Handle Socket.IO connections
 io.on('connection', (socket) => {
   console.log('A client connected');
 
-  // Handle custom event 'trigger'
   socket.on('connectValues', (data) => {
-    // Handle the connectValues event here
     console.log('Received values from client:', data);
 
-    // Now you can pass these values to your MQTT connect function
+    // Pass the values to the MQTT connect function
     connect(data.clientID, data.username, data.password);
+  });
+
+  // Pass back connection information to the website client when connected
+  socket.on('getConnectionInfo', () => {
+    socket.emit('connectionInfoResponse', { clusterURL, port, protocol });
   });
 });
 
-// Start the server
+// Start server
 server.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log('Server running on port 3000');
 });
--- web/main.js
+++ web/main.js
@@ -4,6 +4,7 @@ const menuHitbox = document.querySelector("nav");
 const menuOverlay = document.querySelector(".menu-overlay");
 const closeIcon = document.querySelector(".close-icon");
 
+//  Display menu when hitbox is clicked
 menuHitbox.onclick = function() {
     console.log("Opened menu");
 
@@ -22,19 +23,48 @@ const darkModeToggle = document.getElementById("darkModeToggle");
 const cssRoot = document.querySelector(":root");
 const menuIcon = document.querySelector(".menu-icon");
 
+// Defualt
+const hoverHandler = () => {
+    closeIcon.style.filter = "invert(34%) sepia(77%) saturate(1199%) hue-rotate(321deg) brightness(86%) contrast(82%)";
+};
+
+const mouseoutHandlerLightMode = () => {
+    closeIcon.style.filter = "invert(0%) sepia(39%) saturate(5179%) hue-rotate(36deg) brightness(87%) contrast(88%)";
+};
+
+const mouseoutHandlerDarkMode = () => {
+    closeIcon.style.filter = "invert(99%) sepia(1%) saturate(2039%) hue-rotate(37deg) brightness(119%) contrast(86%)";
+};
+
+closeIcon.addEventListener('mouseover', hoverHandler);
+closeIcon.addEventListener('mouseout', mouseoutHandlerLightMode);
+
+//  Switch between light and dark mode
+// Note: generated CSS filters using https://isotropic.co/tool/hex-color-to-css-filter/
 function switchTheme() {
     if (darkModeToggle.checked == true) {
         console.log("Switching to dark theme");
         cssRoot.style.setProperty("--bg-color", "#0F0F0F");
         cssRoot.style.setProperty("--main-text-color", "#EDEDED");
 
-        // Generated filter using https://isotropic.co/tool/hex-color-to-css-filter/
+        // Remove close icon light mode color
+        closeIcon.removeEventListener('mouseout', mouseoutHandlerLightMode);
+
+        // Add new one
+        closeIcon.addEventListener('mouseout', mouseoutHandlerDarkMode);
+
         menuIcon.style.filter = "invert(99%) sepia(1%) saturate(2039%) hue-rotate(37deg) brightness(119%) contrast(86%)";
         closeIcon.style.filter = "invert(99%) sepia(1%) saturate(2039%) hue-rotate(37deg) brightness(119%) contrast(86%)";
     } else {
         console.log("Switching back to light theme");
         cssRoot.style.setProperty("--bg-color", "#EDEDED");
         cssRoot.style.setProperty("--main-text-color", "#0F0F0F");
+
+        // Remove close icon dark mode color
+        closeIcon.removeEventListener('mouseout', mouseoutHandlerDarkMode);
+
+        // Add new one
+        closeIcon.addEventListener('mouseout', mouseoutHandlerLightMode);
 
         menuIcon.style.filter = "unset";
         closeIcon.style.filter = "unset";
@@ -44,19 +74,35 @@ function switchTheme() {
 //      MQTT connection
 
 const connectForm = document.querySelector(".connect-form");
+const monitorSection = document.querySelector(".monitor-section");
 
 const clientIDInput = document.getElementById("clientId");
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 
+const infoCluster = document.querySelector(".info-cluster");
+const infoPort = document.querySelector(".info-port");
+const infoProtocol = document.querySelector(".info-protocol");
+const infoUsername = document.querySelector(".info-username");
+const infoPassword = document.querySelector(".info-password");
+const infoTime = document.querySelector(".info-time");
+
 function hideConnectForm() {
     connectForm.style.display = "none";
+}
+
+function showDashboard() {
+    monitorSection.style.display = "flex";
 }
 
 function connect() {
     const clientID = clientIDInput.value.toString();
     const username = usernameInput.value.toString();
     const password = passwordInput.value.toString();
+
+    clientIDInput.value = "";
+    usernameInput.value = "";
+    passwordInput.value = "";
 
     //      Socket IO
 
@@ -66,6 +112,22 @@ function connect() {
     // Emit the values to the server
     socket.emit('connectValues', { clientID, username, password });
 
-    // When all done, hide form
-    //hideConnectForm();
+    // Show connection info
+    infoUsername.innerHTML = "Username: " + username;
+    infoPassword.innerHTML = "Password: " + "*".repeat(password.length);
+
+    // Get connection info
+    socket.emit('getConnectionInfo');
+
+    socket.on('connectionInfoResponse', (connectionInfo) => {
+        console.log(connectionInfo.clusterURL + "   " + connectionInfo.port + "   " + connectionInfo.protocol);
+
+        infoCluster.innerHTML = "Cluster URL: " + connectionInfo.clusterURL;
+        infoPort.innerHTML = "Port: " + connectionInfo.port;
+        infoProtocol.innerHTML = "Protocol: " + connectionInfo.protocol;
+    });
+
+    // When all done, hide form + show the other dashboard sections
+    hideConnectForm();
+    showDashboard();
 }
